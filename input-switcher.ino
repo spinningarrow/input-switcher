fca6d15b94e84d07aa55c709925743684ace82c3
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2020-04-11 14:09:16 +0200

    Add support for two more inputs

--- input-switcher.ino
+++ input-switcher.ino
@@ -1,15 +1,21 @@
-int input1 = D3;
-int input2 = D4;
+int input1 = D1;
+int input2 = D2;
+int input3 = D3;
+int input4 = D4;
 
 void setup() {
    pinMode(input1, OUTPUT);
    pinMode(input2, OUTPUT);
+   pinMode(input3, OUTPUT);
+   pinMode(input4, OUTPUT);
 
    Particle.function("led", switchInput);
    Particle.function("input", switchInput);
 
    digitalWrite(input1, LOW);
    digitalWrite(input2, LOW);
+   digitalWrite(input3, LOW);
+   digitalWrite(input4, LOW);
 
    RGB.control(true);
    RGB.brightness(4);
@@ -23,6 +29,8 @@ void loop() {
 int switchInput(String command) {
     digitalWrite(input1, LOW);
     digitalWrite(input2, LOW);
+    digitalWrite(input3, LOW);
+    digitalWrite(input4, LOW);
 
     if (command == "1") {
         digitalWrite(input1, HIGH);
@@ -32,6 +40,16 @@ int switchInput(String command) {
     if (command == "2") {
         digitalWrite(input2, HIGH);
         return 2;
+    }
+
+    if (command == "3") {
+        digitalWrite(input3, HIGH);
+        return 3;
+    }
+
+    if (command == "4") {
+        digitalWrite(input4, HIGH);
+        return 4;
     }
 
     return -1;
