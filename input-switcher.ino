4fb1fbdea11899f7bc21f49c4fe816bcb2435930
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2020-04-13 21:10:48 +0200

    Add RGB indicator LED

--- input-switcher.ino
+++ input-switcher.ino
@@ -3,11 +3,19 @@ int input2 = D2;
 int input3 = D3;
 int input4 = D4;
 
+int red = A0;
+int green = A1;
+int blue = A2;
+
 void setup() {
    pinMode(input1, OUTPUT);
    pinMode(input2, OUTPUT);
    pinMode(input3, OUTPUT);
    pinMode(input4, OUTPUT);
+
+   pinMode(red, OUTPUT);
+   pinMode(green, OUTPUT);
+   pinMode(blue, OUTPUT);
 
    Particle.function("input", switchInput);
 
@@ -15,6 +23,8 @@ void setup() {
    digitalWrite(input2, LOW);
    digitalWrite(input3, LOW);
    digitalWrite(input4, LOW);
+
+   ledWhite();
 
    RGB.control(true);
    RGB.brightness(4);
@@ -33,23 +43,76 @@ int switchInput(String command) {
 
     if (command == "1") {
         digitalWrite(input1, HIGH);
+        ledGreen();
         return 1;
     }
 
     if (command == "2") {
         digitalWrite(input2, HIGH);
+        ledRed();
         return 2;
     }
 
     if (command == "3") {
         digitalWrite(input3, HIGH);
+        ledBlue();
         return 3;
     }
 
     if (command == "4") {
         digitalWrite(input4, HIGH);
+        ledMagenta();
         return 4;
     }
 
-    return -1;
+    ledYellow();
+    return 0;
+}
+
+void ledRed() {
+    digitalWrite(red, LOW);
+    digitalWrite(green, HIGH);
+    digitalWrite(blue, HIGH);
+}
+
+void ledBlue() {
+    digitalWrite(red, HIGH);
+    digitalWrite(green, HIGH);
+    digitalWrite(blue, LOW);
+}
+
+void ledGreen() {
+    digitalWrite(red, HIGH);
+    digitalWrite(green, LOW);
+    digitalWrite(blue, HIGH);
+}
+
+void ledMagenta() {
+    digitalWrite(red, LOW);
+    digitalWrite(green, HIGH);
+    digitalWrite(blue, LOW);
+}
+
+void ledYellow() {
+    digitalWrite(red, LOW);
+    digitalWrite(green, LOW);
+    digitalWrite(blue, HIGH);
+}
+
+void ledCyan() {
+    digitalWrite(red, HIGH);
+    digitalWrite(green, LOW);
+    digitalWrite(blue, LOW);
+}
+
+void ledWhite() {
+    digitalWrite(red, LOW);
+    digitalWrite(green, LOW);
+    digitalWrite(blue, LOW);
+}
+
+void ledOff() {
+    digitalWrite(red, HIGH);
+    digitalWrite(green, HIGH);
+    digitalWrite(blue, HIGH);
 }
