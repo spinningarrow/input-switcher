22c940beb76fe4572b494c55218e45b937ceb239
Sahil Bajaj <1797328+spinningarrow@users.noreply.github.com>
2024-02-05 22:37:36 +0100

    Get latest changes from Particle

--- input-switcher.ino
+++ input-switcher.ino
@@ -3,35 +3,26 @@ int input2 = D2;
 int input3 = D3;
 int input4 = D4;
 
-int red = A0;
-int green = A1;
-int blue = A2;
-
 String currentInput = "0";
 
 void setup() {
+   // WiFi.setCredentials("My_Router", "mypasswordishuge");
+   RGB.control(true);
+   RGB.brightness(0);
+   RGB.control(false);
+    
    pinMode(input1, OUTPUT);
    pinMode(input2, OUTPUT);
    pinMode(input3, OUTPUT);
    pinMode(input4, OUTPUT);
-
-   pinMode(red, OUTPUT);
-   pinMode(green, OUTPUT);
-   pinMode(blue, OUTPUT);
-
-   Particle.function("input", switchInput);
-   Particle.variable("currentInput", currentInput);
-
+   
    digitalWrite(input1, LOW);
    digitalWrite(input2, LOW);
    digitalWrite(input3, LOW);
    digitalWrite(input4, LOW);
-
-   ledWhite();
-
-   RGB.control(true);
-   RGB.brightness(4);
-   RGB.control(false);
+   
+   Particle.function("input", switchInput);
+   Particle.variable("currentInput", currentInput);
 }
 
 void loop() {
@@ -43,81 +34,28 @@ int switchInput(String command) {
     digitalWrite(input2, LOW);
     digitalWrite(input3, LOW);
     digitalWrite(input4, LOW);
-
+    
     currentInput = command;
 
     if (command == "1") {
         digitalWrite(input1, HIGH);
-        ledGreen();
         return 1;
     }
 
     if (command == "2") {
         digitalWrite(input2, HIGH);
-        ledRed();
         return 2;
     }
-
+    
     if (command == "3") {
         digitalWrite(input3, HIGH);
-        ledBlue();
         return 3;
     }
-
+    
     if (command == "4") {
         digitalWrite(input4, HIGH);
-        ledMagenta();
         return 4;
     }
 
-    ledOff();
     return 0;
-}
-
-void ledRed() {
-    digitalWrite(red, LOW);
-    digitalWrite(green, HIGH);
-    digitalWrite(blue, HIGH);
-}
-
-void ledBlue() {
-    digitalWrite(red, HIGH);
-    digitalWrite(green, HIGH);
-    digitalWrite(blue, LOW);
-}
-
-void ledGreen() {
-    digitalWrite(red, HIGH);
-    digitalWrite(green, LOW);
-    digitalWrite(blue, HIGH);
-}
-
-void ledMagenta() {
-    digitalWrite(red, LOW);
-    digitalWrite(green, HIGH);
-    digitalWrite(blue, LOW);
-}
-
-void ledYellow() {
-    digitalWrite(red, LOW);
-    digitalWrite(green, LOW);
-    digitalWrite(blue, HIGH);
-}
-
-void ledCyan() {
-    digitalWrite(red, HIGH);
-    digitalWrite(green, LOW);
-    digitalWrite(blue, LOW);
-}
-
-void ledWhite() {
-    digitalWrite(red, LOW);
-    digitalWrite(green, LOW);
-    digitalWrite(blue, LOW);
-}
-
-void ledOff() {
-    digitalWrite(red, HIGH);
-    digitalWrite(green, HIGH);
-    digitalWrite(blue, HIGH);
 }
